656bbb7849bd9155b6ab2a99145d16a7306d91be
Tushar Imran <tusharimran9909@gmail.com>
2021-03-03 12:24:41 +0600

    remove data change method

--- includes/Installer.php
+++ includes/Installer.php
@@ -5,9 +5,6 @@ class Installer {
     
     public function migrate()
     {
-        if(empty(get_option('simple301redirects_version')) || version_compare(get_option('simple301redirects_version'), SIMPLE301REDIRECTS_VERSION, '<')){
-            $this->option_value_migration_from_serialize_to_json();
-        }
         $this->set_version_number();
     }
 
@@ -16,13 +13,5 @@ class Installer {
         if (get_option('simple301redirects_version') != SIMPLE301REDIRECTS_VERSION) {
 			update_option('simple301redirects_version', SIMPLE301REDIRECTS_VERSION);
 		}
-    }
-
-    public function option_value_migration_from_serialize_to_json()
-    {
-        $option = get_option('301_redirects');
-        if(!empty($option)){
-            update_option('301_redirects', json_encode($option));
-        }
     }
 }
