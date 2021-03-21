e4e99acad0c67f4793eb78e62be3a9dc9b5d1592
Tushar Imran <tusharimran9909@gmail.com>
2021-03-21 14:50:36 +0600

    bug fix - multinetwork plugin action or not checking issue fixing

--- includes/Helper.php
+++ includes/Helper.php
@@ -95,9 +95,6 @@ class Helper {
     }
 
     public static function is_activated_betterlinks(){
-        if(is_multisite()){
-            return is_plugin_active_for_network('betterlinks/betterlinks.php');
-        }
-        return in_array('betterlinks/betterlinks.php', apply_filters('active_plugins', get_option('active_plugins')));
+        return is_plugin_active('betterlinks/betterlinks.php');
     }
 }
