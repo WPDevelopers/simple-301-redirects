3f59b6d99eeb7f87379b3fd78a3fc83b966d396d
Tushar Imran <tusharimran9909@gmail.com>
2021-03-23 10:34:27 +0600

    Compatibility - jwt auth api endpoint whitelist issue

--- includes/API.php
+++ includes/API.php
@@ -5,8 +5,15 @@ class API {
     public function __construct()
     {
         $this->register_settings_rest_API();
+        add_filter('jwt_auth_whitelist', [$this, 'whitelist_API']);
     }
     public function register_settings_rest_API(){
         new API\Settings();
     }
+    public function whitelist_API($endpoints)
+	{
+		$endpoints[] = '/wp-json/simple301redirects/v1/*';
+		$endpoints[] = '/index.php?rest_route=/simple301redirects/v1/*';
+		return $endpoints;
+	}
 }
