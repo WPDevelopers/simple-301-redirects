8b466be1c0ecab0898824a460dd1a3ba68ac89f4
Tushar Imran <tusharimran9909@gmail.com>
2021-03-23 12:31:37 +0600

    notice text update

--- src/components/BetterLinks/index.js
+++ src/components/BetterLinks/index.js
@@ -30,8 +30,11 @@ export default function BetterLinks(props) {
 						<img width="20" src={plugin_root_url + 'assets/images/close.svg'} alt="logo" />
 					</button>
 					<div className="simple301redirects__betterlinks__content">
-						<h3>{__('Betterlinks', 'simple-301-redirects')}</h3>
-						<h4>{__('Install BetterLinks to get the best out of 301 Redirects and get access to more advanced features. Check out the features below', 'simple-301-redirects')}<img width="18" style={{transform: "translateY(6px) scale(1.5)", marginLeft: 8}} src={plugin_root_url + 'assets/images/pointing-down.svg'} alt="logo" /></h4>
+						<h3>{__('BetterLinks – Shorten, Track and Manage any URL', 'simple-301-redirects')}</h3>
+						<h4>
+							{__('Install BetterLinks to get the best out of Simple 301 Redirects and get access to more advanced features. Check out the features below', 'simple-301-redirects')}
+							<img width="18" style={{ transform: 'translateY(6px) scale(1.5)', marginLeft: 8 }} src={plugin_root_url + 'assets/images/pointing-down.svg'} alt="logo" />
+						</h4>
 						<ul>
 							<li>{__('Easy-to-use & Simple Link Shortener', 'simple-301-redirects')}</li>
 							<li>{__('Create attractive looking links instantly', 'simple-301-redirects')}</li>
