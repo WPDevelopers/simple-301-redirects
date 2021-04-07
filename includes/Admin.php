d0ed36cc2b82ff0c921fb8ffe4a5c6895a5ea0f8
Tushar Imran <tusharimran9909@gmail.com>
2021-04-07 11:18:01 +0600

    added WPInsights integration

--- includes/Admin.php
+++ includes/Admin.php
@@ -8,6 +8,7 @@ class Admin {
         $this->load_assets();
         $this->init_ajax();
         $this->init_tools();
+        $this->usage_tracker();
         add_filter('Simple301Redirects/Admin/skip_no_conflict', [$this, 'skip_no_conflict']);
     }
     public function add_menu()
@@ -33,5 +34,21 @@ class Admin {
 			return true;
 		}
 		return false;
-	}
+    }
+    public function usage_tracker()
+    {
+        $tracker = Admin\WPDev\PluginUsageTracker::get_instance( SIMPLE301REDIRECTS_PLUGIN_FILE, [
+            'opt_in'       => true,
+            'goodbye_form' => true,
+            'item_id'      => 'c1e613119bf3e9188767'
+        ] );
+        $tracker->set_notice_options(array(
+            'notice' => __( 'Want to help make <strong>Simple 301 Redirects</strong> even more awesome? if you allow us to track the usage.', 'simple-301-redirects' ),
+            'extra_notice' => __( 'We collect non-sensitive diagnostic data and plugin usage information. 
+            Your site URL, WordPress & PHP version, plugins & themes and email address to send you the 
+            discount coupon. This data lets us make sure this plugin always stays compatible with the most 
+            popular plugins and themes. No spam, I promise.', 'simple-301-redirects' ),
+        ));
+        $tracker->init();
+    }
 }
