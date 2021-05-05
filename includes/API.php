dadd5e35640d2c10a94bc3b14a5018bce8be92d9
Tushar Imran <tusharimran9909@gmail.com>
2021-05-05 15:23:43 +0600

    bug fix - https mixed content issue fixing

--- includes/API.php
+++ includes/API.php
@@ -6,6 +6,7 @@ class API {
     {
         $this->register_settings_rest_API();
         add_filter('jwt_auth_whitelist', [$this, 'whitelist_API']);
+        add_filter( 'rest_url', array($this, 'rest_url_ssl') );
     }
     public function register_settings_rest_API(){
         new API\Settings();
@@ -15,5 +16,12 @@ class API {
 		$endpoints[] = '/wp-json/simple301redirects/v1/*';
 		$endpoints[] = '/index.php?rest_route=/simple301redirects/v1/*';
 		return $endpoints;
-	}
+    }
+    public function rest_url_ssl($url){
+        if ( is_ssl() || ( is_admin() && force_ssl_admin() ) ) {
+                $url = set_url_scheme( $url, 'https' );
+                return $url;
+        }
+        return $url;
+    }
 }
