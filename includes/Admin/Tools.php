b6e8fc24d44d9d73749137dc321c26eac3ff48ee
Tushar Imran <tusharimran9909@gmail.com>
2021-10-12 11:26:47 +0600

    Improved - added CSV import export and remove old JSON import export

--- includes/Admin/Tools.php
+++ includes/Admin/Tools.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 namespace Simple301Redirects\Admin;
 
-class Tools { 
+class Tools
+{
     public function __construct()
     {
         add_action('admin_init', [$this, 'export_data']);
@@ -11,64 +12,88 @@ class Tools {
     public function export_data()
     {
         $page = isset($_GET['page']) ? $_GET['page'] : '';
-		$export = isset($_REQUEST['export']) ? $_REQUEST['export'] : false;
-		if ($page === '301options' && $export == true && current_user_can( 'manage_options' )) {
-			check_ajax_referer('simple301redirects', 'security');
-			$content = json_encode(get_option(SIMPLE301REDIRECTS_SETTINGS_NAME));
-			$filename = 'simple-301-redirects.' . date('Y-m-d') . '.json';
-			($file = fopen($filename, 'w')) or die('Unable to open file!');
-			fwrite($file, $content);
-			fclose($file);
-			header("Content-Disposition: attachment; filename=\"" . $filename . "\"");
-			header('Content-Type: application/force-download');
-			header('Expires: 0');
-			header('Cache-Control: must-revalidate');
-			header('Pragma: public');
-			header('Content-Type: text/plain');
-
-			echo $content;
-			exit();
-		}
+        $export = isset($_REQUEST['export']) ? $_REQUEST['export'] : false;
+        if ($page === '301options' && $export == true && current_user_can('manage_options')) {
+            check_ajax_referer('simple301redirects', 'security');
+            $content = get_option(SIMPLE301REDIRECTS_SETTINGS_NAME);
+            $content = $this->prepare_csv_file_data(get_option(SIMPLE301REDIRECTS_SETTINGS_NAME));
+            $filename = 'simple-301-redirects.' . date('Y-m-d') . '.csv';
+            header('Content-Type: application/csv');
+            header('Content-Disposition: attachment; filename="'.$filename.'";');
+            $f = fopen('php://output', 'w');
+            foreach ($content as $line) {
+                fputcsv($f, $line);
+            }
+            exit();
+        }
+    }
+    public function prepare_csv_file_data($data)
+    {
+        $formatted_data = [];
+        foreach ($data as $key => $value) {
+            $formatted_data[] = [
+                $key,
+                $value
+            ];
+        }
+        if (is_array($data) && count($data) > 0) {
+            return array_merge([['request', 'destination']], $formatted_data);
+        }
+        return [];
     }
     public function import_data()
-	{
-		$page = isset($_GET['page']) ? $_GET['page'] : '';
-		$import = isset($_REQUEST['import']) ? $_REQUEST['import'] : false;
-		if ($page === '301options' && $import == true && current_user_can( 'manage_options' )) {
-			check_ajax_referer('simple301redirects', 'security');
-			if (!empty($_FILES['upload_file']['tmp_name'])) {
-				$fileContent = json_decode(file_get_contents($_FILES['upload_file']['tmp_name']), true);
+    {
+        $page = isset($_GET['page']) ? $_GET['page'] : '';
+        $import = isset($_REQUEST['import']) ? $_REQUEST['import'] : false;
+        if ($page === '301options' && $import == true && current_user_can('manage_options')) {
+            check_ajax_referer('simple301redirects', 'security');
+            $file = $_FILES['upload_file'];
+            if (!empty($file['tmp_name']) && $file['type'] === 'text/csv') {
+                $fileContent = fopen($file['tmp_name'], "r");
                 if (!empty($fileContent)) {
                     $results = $this->process_data($fileContent);
                     $_SESSION['simple_301_redirects_import_info'] = json_encode($results);
-                } 
-			}
-		}
+                }
+            }
+        }
     }
-    public function process_data($data)
-	{
-		$message = '';
-		if (isset($data) && is_array($data) && count($data) > 0) {
+    public function process_data($csv)
+    {
+        $message = '';
+        $count = 0;
+        $data = [];
+        while (($item = fgetcsv($csv)) !== false) {
+            if ($count === 0) {
+                $this->link_header = $item;
+                $count++;
+                continue;
+            }
+            $item = array_combine($this->link_header, $item);
+            $item = \Simple301Redirects\Helper::sanitize_text_or_array_field($item);
+            $data[$item['request']] = $item['destination'];
+        }
+        
+        if (count($data) > 0) {
             $oldData = get_option(SIMPLE301REDIRECTS_SETTINGS_NAME);
-            $value = (!empty($oldData) ? array_unique (array_merge (get_option(SIMPLE301REDIRECTS_SETTINGS_NAME), $data)) : $data);
+            $value = (!empty($oldData) ? array_unique(array_merge(get_option(SIMPLE301REDIRECTS_SETTINGS_NAME), $data)) : $data);
             $restuls = update_option(SIMPLE301REDIRECTS_SETTINGS_NAME, $value);
-            if( $restuls){
+            if ($restuls) {
                 $message = 'All Data has been successfully Imported.';
             } else {
                 $message = 'Import Failed.';
             }
-		}
-		return $message;
+        }
+        return $message;
     }
     public function get_import_info()
-	{
-		check_ajax_referer('simple301redirects', 'security');
-		$results = '';
-		if (isset($_SESSION['simple_301_redirects_import_info'])) {
-			$results = $_SESSION['simple_301_redirects_import_info'];
-			unset($_SESSION['simple_301_redirects_import_info']);
-		}
-		wp_send_json_success($results);
-		wp_die();
-	}
+    {
+        check_ajax_referer('simple301redirects', 'security');
+        $results = '';
+        if (isset($_SESSION['simple_301_redirects_import_info'])) {
+            $results = $_SESSION['simple_301_redirects_import_info'];
+            unset($_SESSION['simple_301_redirects_import_info']);
+        }
+        wp_send_json_success($results);
+        wp_die();
+    }
 }
--- includes/Helper.php
+++ includes/Helper.php
@@ -1,29 +1,31 @@
 <?php
 namespace Simple301Redirects;
 
-class Helper {
+class Helper
+{
     /**
-	 * Check Supported Post type for admin page and plugin main settings page
-	 *
-	 * @return bool
-	 */
+     * Check Supported Post type for admin page and plugin main settings page
+     *
+     * @return bool
+     */
 
-	public static function plugin_page_hook_suffix($hook)
-	{
-		if ($hook == 'settings_page_301options') {
-			return true;
-		}
-		return false;
-	}
-    public static function str_ireplace($search,$replace,$subject){
+    public static function plugin_page_hook_suffix($hook)
+    {
+        if ($hook == 'settings_page_301options') {
+            return true;
+        }
+        return false;
+    }
+    public static function str_ireplace($search, $replace, $subject)
+    {
         $token = chr(1);
         $haystack = strtolower($subject);
         $needle = strtolower($search);
-        while (($pos=strpos($haystack,$needle))!==FALSE){
-            $subject = substr_replace($subject,$token,$pos,strlen($search));
-            $haystack = substr_replace($haystack,$token,$pos,strlen($search));
+        while (($pos=strpos($haystack, $needle))!==false) {
+            $subject = substr_replace($subject, $token, $pos, strlen($search));
+            $haystack = substr_replace($haystack, $token, $pos, strlen($search));
         }
-        $subject = str_replace($token,$replace,$subject);
+        $subject = str_replace($token, $replace, $subject);
         return $subject;
     }
 
@@ -94,7 +96,24 @@ class Helper {
         return $install;
     }
 
-    public static function is_activated_betterlinks(){
+    public static function is_activated_betterlinks()
+    {
         return is_plugin_active('betterlinks/betterlinks.php');
+    }
+
+    public static function sanitize_text_or_array_field($array_or_string)
+    {
+        if (is_string($array_or_string)) {
+            $array_or_string = sanitize_text_field($array_or_string);
+        } elseif (is_array($array_or_string)) {
+            foreach ($array_or_string as $key => &$value) {
+                if (is_array($value)) {
+                    $value = self::sanitize_text_or_array_field($value);
+                } else {
+                    $value = sanitize_text_field($value);
+                }
+            }
+        }
+        return $array_or_string;
     }
 }
